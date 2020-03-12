7d55585edffc557f4ee8233dd1116a70a31962f2
FernandoENF <fernandoengh@gmail.com>
2020-03-12 19:12:17 +0000

    metodos

--- questao1/Lampada.java
+++ questao1/Lampada.java
@@ -1,5 +1,22 @@
 package questao1;
 
 public class Lampada {
+  boolean ligada;
 
+public void liga(){
+  this.ligada = true;
+
+}
+public void desliga(){
+  this.desliga = false;
+
+}
+public String observa(){
+  if (ligada = true){
+    return "ligada";
+  }
+  if (desligada = false){
+    return "desligada";
+  }
+}
 }
